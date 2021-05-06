f93871465a899ee7bcf669fdfc44d4f1fe6cd5b2
andrebluz <andreluzawp@gmail.com>
2021-05-06 20:32:01 -0300

    Simple script for flocking behaviour

---
+++ Assets/Scripts/FLOCK.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLOCK : MonoBehaviour
+{
+    public FLOCKMANAGER myManager; //pega o manager *prefab não pega
+    private GameObject _FM; //intermediador para captar o manager caso o gameobject associado a esse script seja um prefab
+    float speed; 
+
+    private void Awake()
+    {
+
+        //originalmente os prefabs não tinha o MANAGER associado a eles, já que o MANAGER estava em CENA,
+        //criei uma variável privada _FM que se identifiicar o myManager como nulo(1), associa a variável _FM ao manager(2) que anteriormente era impossível ser enxergado por um prefab
+        //faz a captação do script do manager em CENA "GetComponent<FLOCKMANAGER>" (3)
+        if (myManager == null) //(1)
+        {
+            _FM = GameObject.FindGameObjectWithTag("_FM"); //(2)
+            myManager = _FM.GetComponent<FLOCKMANAGER>(); //(3)
+        }
+    }
+
+    private void Start()
+    {
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed); //velocide de swmming individual do flocking
+    }
+
+    private void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/FLOCKMANAGER.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLOCKMANAGER : MonoBehaviour
+{
+
+    public GameObject fishPrefab; //prefabs para instanciar no flocking
+    public int numFish = 20; //numero de swimmingers
+    public GameObject[] allFishs;
+    public Vector3 swinLimits = new Vector3(5, 5, 5);
+
+    [Header("Configurações do Cardume")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed; //velocidade miníma
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed; //velocidade máxima
+
+    private void Start()
+    {
+        allFishs = new GameObject[numFish]; //tamanho do array
+        for(int i = 0; i < numFish; i++)
+        {
+            //posição dentro dos limites para casa instancia
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x),
+                                                                Random.Range(-swinLimits.y, swinLimits.y),
+                                                                Random.Range(-swinLimits.z, swinLimits.z));
+
+            allFishs[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity); //instancia os prefabs
+        }
+    }
+
+}
